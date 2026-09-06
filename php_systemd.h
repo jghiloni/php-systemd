a94b519dfc6bfd78b825a514d7b8b67543df44de
Josh Ghiloni <github@joshghiloni.me>
2026-09-06 15:25:02 -0400

    Include PR #8 from upstream

--- php_systemd.h
+++ php_systemd.h
@@ -1,11 +1,15 @@
 #ifndef PHP_SYSTEMD_H
 #define PHP_SYSTEMD_H 1
-#define PHP_SYSTEMD_VERSION "0.1.2"
+#define PHP_SYSTEMD_VERSION "0.2.0"
 #define PHP_SYSTEMD_EXTNAME "systemd"
 
 PHP_FUNCTION(sd_journal_send);
 
 extern zend_module_entry systemd_module_entry;
 #define phpext_systemd_ptr &systemd_module_entry
+
+#ifndef SD_JOURNAL_SUPPRESS_LOCATION
+#define SD_JOURNAL_SUPPRESS_LOCATION 1
+#endif
 
 #endif
